eb7d91ddf10de1ef9af2d6886a52cea85bb211a5
Seung jin Ham <109064686+Hamseungjin@users.noreply.github.com>
2024-05-22 20:07:26 +0900

    'testcode.py수정'

--- testcode.py
+++ testcode.py
@@ -1,7 +1,15 @@
 # 따릉이 API URL
+import requests
+
 def request_seoul_api(api_key, start_index, end_index):
-    g_api_host = "http://openapi.seoul.go.kr:8088"
-    g_type = "json"
-    g_service = "bikeList"
-    url = f"{g_api_host}/{api_key}/{g_type}/{g_service}/{start_index}/{end_index}/"
+    # API 요청 URL
+    url = f"http://openapi.seoul.go.kr:8088/{api_key}/json/bikeList/{start_index}/{end_index}/"
+    try:
+        print(f"Requesting URL: {url}")  # URL 로그 출력
+        response = requests.get(url)
+        print(f"Response: {response.status_code}")  # 응답 코드 로그 출력
+        return response
+    except Exception as e:
+        print(f"Error occurred in request_seoul_api: {e}")
+        return None
 
