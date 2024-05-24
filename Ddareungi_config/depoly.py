c9f459988a2ae49c2c5df0a2ffe314167ebbecd1
Seung jin Ham <109064686+Hamseungjin@users.noreply.github.com>
2024-05-24 21:21:05 +0900

    [승진][sprint 3] depoly.py 수정(producer.py만 실행 + 로그 수집X)

--- Ddareungi_config/depoly.py
+++ Ddareungi_config/depoly.py
@@ -1,9 +1,8 @@
 import subprocess
 
 def run_producer():
-    """producer.py를 백그라운드에서 실행하고 로그를 파일로 저장합니다."""
-    """  스크립트 실행 중 발생하는 모든 표준 출력과 표준 오류 메시지가 /src/kafka-producer/producer.log 파일에 기록 """
-    command = "nohup python /src/kafka-producer/producer.py > /src/kafka-producer/producer.log 2>&1 &"
+    """ subprocess.run 함수의 check=True 옵션은 명령어 실행이 실패할 경우 CalledProcessError 예외를 발생시키므로 스크립트가 예외 처리"""
+    command = "python /src/kafka-producer/producer.py"
     subprocess.run(command, shell=True, check=True)
 
 if __name__ == '__main__':
