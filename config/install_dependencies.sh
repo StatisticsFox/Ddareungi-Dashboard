458c134632d57d439b1494eb740787a07f1473a6
CHOI_JEE_HYUK <92065443+StatisticsFox@users.noreply.github.com>
2024-05-25 00:25:11 +0900

    [지혁][sprint 3] Create install_dependencies.sh

---
+++ config/install_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 경로 설정
+PROJECT_DIR=/src/kafka-producer
+VENV_DIR=$PROJECT_DIR/venv
+
+# 가상 환경 디렉토리가 존재하지 않으면 생성
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv $VENV_DIR
+fi
+
+# 가상 환경 활성화
+source $VENV_DIR/bin/activate
+
+# 필요 패키지 설치
+pip install -r $PROJECT_DIR/requirements.txt
+
+echo "Dependencies installed."
