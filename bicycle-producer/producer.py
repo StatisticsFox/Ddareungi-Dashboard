8dae8f2fe8f84e467a418a6cdce0d049537a2394
CHOI JEE HYUK <akfktl328@kyongi.ac.kr>
2024-05-24 15:25:05 +0900

    api 호출함수 수정

--- bicycle-producer/producer.py
+++ bicycle-producer/producer.py
@@ -12,7 +12,13 @@ producer = KafkaProducer(bootstrap_servers=['kafka_node1:9092', 'kafka_node2:909
 value_serializer=lambda x: json.dumps(x).encode("utf-8"))
 
 # api 불러오는 함수
-
+def request_seoul_api(api_key, start_index, end_index):
+	"""서울 열린 데이터 광장 API를 호출하여 따릉이 정보를 가져온다."""
+	g_api_host = "http://openapi.seoul.go.kr:8088"
+	g_type = "json"
+	g_service = "bikeList"
+	url = f"{g_api_host}/{api_key}/{g_type}/{g_service}/{start_index}/{end_index}/"
+	return requests.get(url)
   
 
 # API 키 읽기
