c7a4a1d0273bf9e2af1b775cb59286c7c3c35576
CHOI_JEE_HYUK <92065443+StatisticsFox@users.noreply.github.com>
2024-05-25 11:38:26 +0900

    Update depoly.py

--- config/depoly.py
+++ config/depoly.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_producer():
     """ subprocess.run 함수의 check=True 옵션은 명령어 실행이 실패할 경우 CalledProcessError 예외를 발생시키므로 스크립트가 예외 처리"""
-    command = "python3 src/kafka-producer/producer.py"
+    command = "python3 home/ubuntu/kafka-producer/producer.py"
     subprocess.run(command, shell=True, check=True)
 
 if __name__ == '__main__':
