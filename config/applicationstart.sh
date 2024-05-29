9bdfce50b0b566221233552f4c6e8035309772fa
Seung jin Ham <109064686+Hamseungjin@users.noreply.github.com>
2024-05-29 10:25:34 +0900

    [승진][sprint 3] applicationstart.sh 수정(주석 추가)

--- config/applicationstart.sh
+++ config/applicationstart.sh
@@ -7,7 +7,7 @@ target_ip="172.31.30.11"
 # IP 주소가 목표 IP 주소와 일치하는지 확인
 if [ "$current_ip" == "$target_ip" ]; then
     # 일치하면 Python 스크립트 실행
-    python3 /home/ubuntu/kafka-producer/bicycle-producer/producer.py
+    # python3 /home/ubuntu/kafka-producer/bicycle-producer/producer.py
 else
     # 일치하지 않으면 패스
     echo "IP address does not match. Skipping script execution."
