c78cded1d2ddc1479c06516e2d256fc4ec3d9353
Park Jihyun <133860852+meowwg@users.noreply.github.com>
2024-05-30 03:07:24 +0900

    [지현][sprint 3] flush() 주석처리 해제

--- bicycle-producer/producer.py
+++ bicycle-producer/producer.py
@@ -88,7 +88,7 @@ def send_data():
                 # 전송한 데이터를 출력
                 print(f"Sent data to Kafka: {data}")
             
-            # producer.flush() # Kafka Producer 종료
+            producer.flush() # Kafka Producer 종료
             # 30초 대기
             time.sleep(30) # 30초마다 실행
 
