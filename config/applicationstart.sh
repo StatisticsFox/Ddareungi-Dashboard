62c3c76f6d559b3577c1619b035323d3add5ff1d
CHOI_JEE_HYUK <92065443+StatisticsFox@users.noreply.github.com>
2024-05-31 19:29:36 +0900

    [지혁][sprint 3] 비교연산자 수정

--- config/applicationstart.sh
+++ config/applicationstart.sh
@@ -5,7 +5,7 @@ current_ip=$(hostname -I | awk '{print $1}')
 target_ip="172.31.30.11"
 
 # IP 주소가 목표 IP 주소와 일치하는지 확인
-if [ "$current_ip" == "$target_ip" ]; then
+if [ "$current_ip" = "$target_ip" ]; then
     # 일치하면 Python 스크립트 실행
     python3 /home/ubuntu/kafka-producer/bicycle-producer/producer.py
 else
